aa51f86095003ff178bef112e2a1bb70bec5b939
Peter Hultqvist <phq@silentorbit.com>
2014-01-14 21:11:48 +0100

    Made ComplexSelector creation public.

---
+++ ExCSS/Model/Selector/CombinatorSelector.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ExCSS
+{
+    public struct CombinatorSelector
+    {
+        public SimpleSelector Selector;
+        public Combinator Delimiter;
+
+        public CombinatorSelector(SimpleSelector selector, Combinator delimiter)
+        {
+            this.Selector = selector;
+            this.Delimiter = delimiter;
+        }
+
+        public char Char
+        {
+            get{
+                switch (Delimiter)
+                {
+                    case Combinator.Child:
+                        return Specification.GreaterThan;
+
+                    case Combinator.AdjacentSibling:
+                        return Specification.PlusSign;
+
+                    case Combinator.Descendent:
+                        return Specification.Space;
+
+                    case Combinator.Sibling:
+                        return Specification.Tilde;
+
+                    default:
+                        throw new NotImplementedException("Unknown combinator: " + Delimiter);
+                }
+            }
+        }
+    }
+
+}
+

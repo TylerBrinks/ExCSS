f94a34d1515f33fece0f066ddb5adb3348beb49e
H1Gdev <h1g.z.ouroboros+github@gmail.com>
2021-12-23 11:12:57 +0900

    Add Strong Name Signing.

--- src/ExCSS/Properties/AssemblyInfo.cs
+++ src/ExCSS/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ExCSS")]
 [assembly: AssemblyTrademark("")]
-[assembly: InternalsVisibleTo("ExCSS.Tests")]
+[assembly: InternalsVisibleTo("ExCSS.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100816d68fc780a643f4de3d073378928c09d763309e14aec27af5d9ce67f7bdcf824b9e82dbf4edd3d2f833c615aec1365d02d4fcab8997c1e1da83916369fcbe6f6d4574b5744790a3f05e617c5d2e74fc95c28e45ad962457b1535c9e0319e121be5d4d00133751b70408205ece1e8718a87e1829449d4926167e6063d2e06c8")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
